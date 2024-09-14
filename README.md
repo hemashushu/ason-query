f7662b9f6e53691e3b144001990a82f1a9708398
Hippo Spark (Arch) <hippospark@gmail.com>
2024-09-14 08:19:14 +0800

    simplified the 'about'

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 _ASON Query_ is a powerful tool for processing ASON data. It can query, manipulate, and generate new ASON data.
 
 ASON Query also serves as an interpreter for the _ASON Query Language (AQL)_.
+
+<!-- @import "[TOC]" {cmd="toc" depthFrom=2 depthTo=6 orderedList=false} -->
+
+<!-- code_chunk_output -->
+
+- [它是如何工作的？](#它是如何工作的)
+
+<!-- /code_chunk_output -->
+
+## 它是如何工作的？
+
+_ASON Query_ （以下简称 _aq_）从指定的文件（或者 标准输入 stdin、管道 pipe）读取 ASON 数据，然后根据查询表达式（query expression）过滤和构建新的 ASON 数据，并输出到指定的文件（或者标准输出 stdout、管道）。
+
--- src/main.rs
+++ src/main.rs
@@ -9,44 +9,13 @@ use std::{
     process,
 };
 
-// use argh::FromArgs;
-use ason::{parse_from_str, print_to_string, print_to_writer};
+use ason::ast::{
+    parser::parse_from_str,
+    printer::{print_to_string, print_to_writer},
+};
 use clap::Parser;
 
-// #[derive(FromArgs, Debug)]
-
 /// ASON Query is a powerful tool for querying, manipulating and generating ASON data.
-///
-/// Examples:
-///
-/// - Read text from STDIN and apply the query, and then write the result to STDOUT.
-///
-///   $ echo '{id: 123, name: "John"} | aq '.id'
-///
-/// - Read text from a specify file and apply the query, and then write the result to STDOUT.
-///
-///   $ aq -i sales.ason '.orders[].id'
-///
-///   $ cat sales.ason | aq '.orders[].id'
-///
-///   $ aq '.orders[].id' < sales.ason
-///
-/// - Read text from STDIN and apply the query, and then write the result to the specify file.
-///
-///   $ echo '[11, 13, 17, 19]' | aq -o numbers.ason '.[].filter(. > 13)'
-///
-///   $ echo '[11, 13, 17, 19]' | aq '.[].filter(. > 13)' > numbers.ason
-///
-/// - Read text from STDIN, read the query string from the specify file and apply
-///   the query, and then write the result to STDOUT.
-///
-///   $ echo '[11, 13, 17, 19]' | aq -q example.aql
-///
-/// Note:
-///
-/// - The STDIN will be omitted if INPUT_FILE is specified.
-///
-/// - The INPUT_FILE will be omitted if INPUT_TEXT is specified.
 ///
 /// Resources:
 ///
@@ -56,83 +25,92 @@ use clap::Parser;
 /// - The ASON Query Language:
 ///   https://hemashushu.github.io/works/ason-query/
 ///
+/// - The ASON Query Examples:
+///   https://github.com/hemashushu/ason-query/examples
+///
 #[derive(Parser, Debug)]
 #[command(name = "aq")]
 #[command(version, about)]
+// #[command()]
 struct AqArgs {
-    // #[argh(option, short='i')]
-
     /*
     To specify the flags for an argument, you can use #[arg(short = 'n')] and/or #[arg(long = "name")]
     attributes on a field. When no value is given (e.g. #[arg(short)]),
     the flag is inferred from the field’s name.
     https://docs.rs/clap/latest/clap/_derive/_tutorial/chapter_2/index.html#options
      */
-    /// Specify the input file
-    #[arg(short, long, value_name = "INPUT_FILE")]
-    input: Option<String>,
-
-    // #[argh(option, short='o')]
     /// Specify the output file
     #[arg(short, long, value_name = "OUTPUT_FILE")]
     output: Option<String>,
 
-    // #[argh(option, short='q')]
+    /* the query expression will be omitted if the query file is specified */
     /// Specify the query file
     #[arg(short, long, value_name = "QUERY_FILE")]
     query: Option<String>,
 
-    // #[argh(option, short='t')]
-    /// Specify the input text
-    #[arg(short, long, value_name = "INPUT_TEXT")]
-    text: Option<String>,
+    /*
+    a default value can be specified:
+    https://docs.rs/clap/latest/clap/_derive/_tutorial/chapter_2/index.html#defaults
+     */
+    /// The query expression
+    query_expression: Option<String>,
 
-    // #[argh(positional, default = "String::from(\".\")")]
-    /// The query string
-    // #[arg(default_value_t = String::from("."))]
-    query_string: Option<String>,
+    /// Specify the input file(s)
+    input_files: Vec<String>,
 }
 
 fn main() {
-    // Usage:  aq [options] <query string>
-    //         aq [options] -i <input file> <query string>
-    //         aq [options] -i <input file> -o <output file> <query string>
-    //         aq [options] -i <input file> -o <output file> -q <query file>
-    //         aq [options] -t <input text> <query string>
+    // Usage:
+    //   aq [options] <query expression> <input file(s)>
+    //   aq [options] -o <output file> <query expression> <input file(s)>
+    //   aq [options] -o <output file> -q <query file> <input file(s)>
     //
     // Command options:
-    //   -i, --input=FILE       specify the input file
     //   -o, --output=FILE      specify the output file
     //   -q, --query=FILE       specify the query file
-    //   -t, --text=TEXT        specify the input text
 
-    // run with Cargo
+    // Run with Cargo
     // --------------
     //
     // `$ cargo run -- <args and options>`
     //
     // e.g.
-    // `cargo run -- -i examples/01-primitive.ason .`
-    // `cargo run -- -i examples/06-error.ason`
+    // `cargo run -- . examples/01-primitive.ason`
     //
-    // run standalone
+    // Run standalone
     // --------------
     //
-    // `aq -i examples/01-primitive.ason .`
-    // `aq -i examples/06-error.ason`
+    // `aq . examples/01-primitive.ason`
     //
-    // read text from STDIN
-    // ---------------
-    // `$ echo '{id: 123}' | aq '.id'`
+    // Examples
+    // --------
     //
-    // ```sh
-    // cat << EOF | aq '.id'
-    // > {
-    // >   id: 123
-    // >   name: "John"
-    // > }
-    // > EOF
-    //```
+    // - Read text from STDIN and apply the query, and then write the result to STDOUT.
+    //
+    //   $ echo '{id: 123, name: "John"} | aq '.id'
+    //
+    //   $ cat << EOF | aq '.id'
+    //   > {
+    //   >   id: 123
+    //   >   name: "John"
+    //   > }
+    //   > EOF
+    //
+    // - Read text from a specify file and apply the query, and then write the result to STDOUT.
+    //
+    //   $ aq '.orders[].id' sales.ason
+    //   $ cat sales.ason | aq '.orders[].id'
+    //   $ aq '.orders[].id' < sales.ason
+    //
+    // - Read text from STDIN and apply the query, and then write the result to the specify file.
+    //
+    //   $ echo '[11, 13, 17, 19]' | aq -o numbers.ason '.filter(. > 13)'
+    //   $ echo '[11, 13, 17, 19]' | aq '.filter(. > 13)' > numbers.ason
+    //
+    // - Read text from STDIN, read the query expression from the specify file and apply
+    //   the query, and then write the result to STDOUT.
+    //
+    //   $ echo '[11, 13, 17, 19]' | aq -q example.aql
 
     // let mut args = std::env::args();
     // https://doc.rust-lang.org/cargo/reference/environment-variables.html
@@ -140,28 +118,39 @@ fn main() {
     // println!("{}", title);
     // process::exit(1);
 
-    // let aq_args:AqArgs = argh::from_env();
-
     let aq_args = AqArgs::parse();
 
-    let source = if let Some(t) = aq_args.text {
-        // text from arg
-        t
-    } else if let Some(f) = aq_args.input {
-        // text from input file
-        match std::fs::read_to_string(&f) {
-            Ok(s) => s,
-            Err(e) => {
-                eprintln!("Fail to read the specified input file: \"{}\".", &f);
-                eprintln!("{}", e);
-                process::exit(1);
+    // Note:
+    //
+    // - The STDIN will be omitted if INPUT_FILES is specified.
+    // - The QUERY_EXPRESSION will be omitted if QUERY_FILE is specified.
+    // - The STDOUT will be omitted if OUTPUT_FILE is specified.
+
+    let mut texts = vec![];
+
+    if !aq_args.input_files.is_empty() {
+        // if let Some(f) = aq_args.inputs {
+
+        println!("{:?}", aq_args.input_files);
+
+        for f in aq_args.input_files {
+            // text from input file
+            match std::fs::read_to_string(&f) {
+                Ok(s) => {
+                    texts.push(s);
+                }
+                Err(e) => {
+                    eprintln!("Fail to read the specified input file: \"{}\".", &f);
+                    eprintln!("{}", e);
+                    process::exit(1);
+                }
             }
         }
     } else {
         // text from STDIN
         let mut i = std::io::stdin().lock();
-        if i.is_terminal() && aq_args.query_string.is_none() {
-            eprintln!("Usage: aq [OPTIONS] [QUERY_STRING]");
+        if i.is_terminal() && aq_args.query_expression.is_none() {
+            eprintln!("Usage: aq [OPTIONS] [QUERY_EXPRESSION]");
             eprintln!();
             eprintln!("For more information, try '--help'.");
             process::exit(1);
@@ -169,7 +158,9 @@ fn main() {
 
         let mut buf = String::new();
         match i.read_to_string(&mut buf) {
-            Ok(_) => buf,
+            Ok(_) => {
+                texts.push(buf);
+            }
             Err(e) => {
                 eprintln!("Fail to read the input text from STDIN.");
                 eprintln!("{}", e);
@@ -178,16 +169,28 @@ fn main() {
         }
     };
 
-    let node = match parse_from_str(&source) {
-        Ok(n) => n,
-        Err(e) => {
-            eprintln!("{}", e.with_source(&source));
-            process::exit(1);
+    let mut nodes = vec![];
+
+    for text in texts {
+        match parse_from_str(&text) {
+            Ok(n) => {
+                nodes.push(n);
+            }
+            Err(e) => {
+                eprintln!("{}", e.with_source(&text));
+                process::exit(1);
+            }
         }
+    }
+
+    let root = if nodes.len() == 1 {
+        nodes.remove(0)
+    } else {
+        ason::ast::AsonNode::Tuple(nodes)
     };
 
     if let Some(f) = aq_args.output {
-        match std::fs::write(&f, print_to_string(&node)) {
+        match std::fs::write(&f, print_to_string(&root)) {
             Err(e) => {
                 eprintln!("Fail to write to the output file: \"{}\".", f);
                 eprintln!("{}", e);
@@ -199,7 +202,7 @@ fn main() {
         }
     } else {
         let mut w = std::io::stdout().lock();
-        match print_to_writer(&mut w, &node) {
+        match print_to_writer(&mut w, &root) {
             Err(e) => {
                 eprintln!("Fail to write to the STDOUT.");
                 eprintln!("{}", e);
